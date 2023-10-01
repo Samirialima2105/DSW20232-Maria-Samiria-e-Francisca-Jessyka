22182093c3a0e68feb02ed82a2925a417f642472
Samirialima2105 <146040355+Samirialima2105@users.noreply.github.com>
2023-10-01 19:40:44 -0300

    Cadastroservlet.java

--- src/main/java/CadastroServlet/Cadastroservlet.java
+++ src/main/java/CadastroServlet/Cadastroservlet.java
@@ -8,23 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class cadastroservlet
- */
 @WebServlet(urlPatterns="/cadastro")
 public class Cadastroservlet extends HttpServlet {
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //O valor do campo de entrada é recuperado como uma string e armazenado na variável modelo
-		//esse código está extraindo informações de um formulário HTML submetido pelo cliente
-		//Ele coleta essas informações como strings e, no caso do "ano de fabricação", converte-o em um número inteiro. 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
         String modelo = request.getParameter("modelo");
         String marca = request.getParameter("marca");
         Integer anoFabricacao = Integer.parseInt(request.getParameter("anoFabricacao")); 
-
-        //está criando uma instância da classe Carro e definindo seus atributos com os valores obtidos anteriormente
-        //Isso significa que você está criando um novo carro(objeto) em memória que pode ser usado para armazenar informações sobre um veículo.
-        //Esses valores são coletados anteriormente a partir dos parâmetros da solicitação HTTP e estão sendo usados 
-        //para definir as características do objeto carro. 
+		
         Carro carro = new Carro();
         carro.setModelo(modelo);
         carro.setMarca(marca);
@@ -32,10 +22,6 @@ public class Cadastroservlet extends HttpServlet {
 
  
         PrintWriter out = response.getWriter();
-
-        // O código que você forneceu está gerando uma página HTML como resposta a uma solicitação HTTP. 
-        //você gera páginas dinâmicas com informações específicas com base nos dados do sistema ou nas entradas do usuário. 
-        //a página está exibindo uma mensagem de sucesso de cadastro e os detalhes do carro cadastrado.
         out.write("<html>"
 		+ "<head>"
 		+ "<meta charset=\"ISO-8859-1\">"
